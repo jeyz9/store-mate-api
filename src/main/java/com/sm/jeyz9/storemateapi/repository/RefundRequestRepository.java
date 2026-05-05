528f92c46d7cd9bdabc6a315f2f4ed7ac7832ae0
jeyz9 <ggjj4511@gmail.com>
2026-05-05 20:09:53 +0700

    chore: update send refund

--- src/main/java/com/sm/jeyz9/storemateapi/repository/RefundRequestRepository.java
+++ src/main/java/com/sm/jeyz9/storemateapi/repository/RefundRequestRepository.java
@@ -2,8 +2,15 @@ package com.sm.jeyz9.storemateapi.repository;
 
 import com.sm.jeyz9.storemateapi.models.RefundRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RefundRequestRepository extends JpaRepository<RefundRequest, Long> {
+    
+    @Query(value = """
+        SELECT COUNT(*) > 0 FROM refund_requests WHERE status = :status AND order_id = :orderId;
+    """, nativeQuery = true)
+    boolean existsByStatusAndOrderId(@Param("status") String status, @Param("orderId") Long orderId);
 }
