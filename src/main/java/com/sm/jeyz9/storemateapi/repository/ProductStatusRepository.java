2792eda25aa77e5419440fcbf99ac966b593531d
jeyz9 <ggjj4511@gmail.com>
2026-06-19 02:26:10 +0700

    chore: add status deleted for product status

--- src/main/java/com/sm/jeyz9/storemateapi/repository/ProductStatusRepository.java
+++ src/main/java/com/sm/jeyz9/storemateapi/repository/ProductStatusRepository.java
@@ -2,8 +2,17 @@ package com.sm.jeyz9.storemateapi.repository;
 
 import com.sm.jeyz9.storemateapi.models.ProductStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ProductStatusRepository extends JpaRepository<ProductStatus, Long> {
+    
+    @Query(value = """
+        SELECT * FROM product_status WHERE status = :status
+    """, nativeQuery = true)
+    Optional<ProductStatus> findByStatus(@Param("status") String status);
 }
