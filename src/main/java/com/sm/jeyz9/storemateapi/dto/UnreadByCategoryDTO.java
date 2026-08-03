94c539b66e0b1f0b967d2b83196b046911d2b23a
jeyz9 <ggjj4511@gmail.com>
2026-08-03 18:05:52 +0700

    chore: add getter, setter and constructor

--- src/main/java/com/sm/jeyz9/storemateapi/dto/UnreadByCategoryDTO.java
+++ src/main/java/com/sm/jeyz9/storemateapi/dto/UnreadByCategoryDTO.java
@@ -1,7 +1,15 @@
 package com.sm.jeyz9.storemateapi.dto;
 
 import com.sm.jeyz9.storemateapi.models.NotifyTypeName;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class UnreadByCategoryDTO {
     private NotifyTypeName notifyType;
     private Integer unread;
